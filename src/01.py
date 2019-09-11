73bfa8335d40d4e91997acd892b3eeaef74bfe75
Evgeny Goldin <goldin@amazon.com>
2019-09-10 22:13:21 -0700

    First appearance - binary walking back

--- src/01.py
+++ src/01.py
@@ -2,10 +2,22 @@ import unittest
 
 class TestBinarySearch(unittest.TestCase):
 
-    @staticmethod
-    def first_appearance(A, k):
-      left  = 0
-      right = len(A) - 1
+    def first_appearance(self, A, k, left = 0, right = -1):
+
+      if  right == -1:
+        right = len(A) - 1
+
+      median = self.binary_search(A, k, left, right)
+
+      if median < 1 or A[median - 1] != k:
+        return median
+
+      return self.first_appearance(A, k, 0, median - 1)
+
+    def binary_search(self, A, k, left, right):
+
+      if  left < 0 or left > right:
+        return -1
 
       while left <= right:
         middle = int(left + ((right - left) / 2))
@@ -13,9 +25,7 @@ class TestBinarySearch(unittest.TestCase):
         if A[middle] < k:
           left = middle + 1
         elif A[middle] == k:
-          while middle > -1 and A[middle] == k:
-            middle = middle - 1
-          return middle + 1
+          return middle
         else:
           right = middle - 1
 
@@ -29,6 +39,8 @@ class TestBinarySearch(unittest.TestCase):
         self.assertEqual(self.first_appearance([1, 2, 3, 3, 4], -5), -1)
 
     def test_single_appearance(self):
+        self.assertEqual(self.first_appearance([1], 1), 0)
+        self.assertEqual(self.first_appearance([1, 1], 1), 0)
         self.assertEqual(self.first_appearance([1, 2, 3, 4, 5], 1), 0)
         self.assertEqual(self.first_appearance([1, 2, 3, 4, 5], 2), 1)
         self.assertEqual(self.first_appearance([1, 2, 3, 4, 5], 3), 2)
