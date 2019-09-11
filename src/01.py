89db0902d3eea6fda1169ae341e491a9ef0b12e7
Evgeny Goldin <goldin@amazon.com>
2019-09-10 22:45:08 -0700

    first_larger added

--- src/01.py
+++ src/01.py
@@ -2,6 +2,7 @@ import unittest
 
 class TestBinarySearch(unittest.TestCase):
 
+    # 1.2
     def first_appearance(self, A, k, left = 0, right = -1):
 
       if  right == -1:
@@ -14,6 +15,24 @@ class TestBinarySearch(unittest.TestCase):
 
       return self.first_appearance(A, k, 0, median - 1)
 
+    # 1.3
+    def first_larger(self, A, k, left = 0, right = -1):
+
+      if  right == -1:
+        right = len(A) - 1
+
+      median = self.binary_search(A, k, left, right)
+
+      if median < 0 or median == right:
+        return -1
+
+      if A[median + 1] != k:
+        return median + 1
+
+      return self.first_larger(A, k, median + 1, right)
+
+
+    # Finds an index of the k in A, or -1
     def binary_search(self, A, k, left, right):
 
       if  left < 0 or left > right:
@@ -31,14 +50,25 @@ class TestBinarySearch(unittest.TestCase):
 
       return -1
 
-    def test_missing(self):
+    def test_first_appearance_missing(self):
         self.assertEqual(self.first_appearance([], 0), -1)
         self.assertEqual(self.first_appearance([], 10), -1)
         self.assertEqual(self.first_appearance([1, 2, 3], 4), -1)
         self.assertEqual(self.first_appearance([1, 2, 3, 3, 4], 5), -1)
         self.assertEqual(self.first_appearance([1, 2, 3, 3, 4], -5), -1)
 
-    def test_single_appearance(self):
+    def test_first_larger_missing(self):
+        self.assertEqual(self.first_larger([], 0), -1)
+        self.assertEqual(self.first_larger([], 10), -1)
+        self.assertEqual(self.first_larger([0], 0), -1)
+        self.assertEqual(self.first_larger([0], 1), -1)
+        self.assertEqual(self.first_larger([1, 2, 3, 4], 4), -1)
+        self.assertEqual(self.first_larger([1, 2, 3, 3, 4], 5), -1)
+        self.assertEqual(self.first_larger([1, 2, 3, 4, 5], 5), -1)
+        self.assertEqual(self.first_larger([1, 2, 3, 3, 4, 5], 5), -1)
+        self.assertEqual(self.first_larger([1, 2, 3, 3, 4], -5), -1)
+
+    def test_first_appearance_single(self):
         self.assertEqual(self.first_appearance([0], 0), 0)
         self.assertEqual(self.first_appearance([1], 1), 0)
         self.assertEqual(self.first_appearance([0, 1], 0), 0)
@@ -49,7 +79,15 @@ class TestBinarySearch(unittest.TestCase):
         self.assertEqual(self.first_appearance([1, 2, 3, 4, 5], 4), 3)
         self.assertEqual(self.first_appearance([1, 2, 3, 4, 5], 5), 4)
 
-    def test_multiple_appearances(self):
+    def test_first_larger_single(self):
+        self.assertEqual(self.first_larger([0, 1], 0), 1)
+        self.assertEqual(self.first_larger([1, 2], 1), 1)
+        self.assertEqual(self.first_larger([1, 2, 3, 4, 5], 1), 1)
+        self.assertEqual(self.first_larger([1, 2, 3, 4, 5], 2), 2)
+        self.assertEqual(self.first_larger([1, 2, 3, 4, 5], 3), 3)
+        self.assertEqual(self.first_larger([1, 2, 3, 4, 5], 4), 4)
+
+    def test_first_appearance_multiple(self):
         self.assertEqual(self.first_appearance([0, 0, 0, 1], 0), 0)
         self.assertEqual(self.first_appearance([0, 0, 0, 1, 1, 1, 1], 1), 3)
         self.assertEqual(self.first_appearance([0, 0, 0, 1, 1, 1, 1, 2, 2, 2, 2], 1), 3)
@@ -58,6 +96,16 @@ class TestBinarySearch(unittest.TestCase):
         self.assertEqual(self.first_appearance([1, 2, 2, 2, 2, 2, 2, 3, 4, 5], 2), 1)
         self.assertEqual(self.first_appearance([1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 3, 4, 5], 2), 5)
         self.assertEqual(self.first_appearance([1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 3, 4, 4, 4, 4, 4, 5], 4), 12)
+
+    def test_first_larger_multiple(self):
+        self.assertEqual(self.first_larger([0, 0, 0, 1], 0), 3)
+        self.assertEqual(self.first_larger([0, 0, 0, 1, 1, 1, 1], 1), -1)
+        self.assertEqual(self.first_larger([0, 0, 0, 1, 1, 1, 1, 2, 2, 2, 2], 1), 7)
+        self.assertEqual(self.first_larger([0, 0, 0, 1, 1, 1, 1, 2, 2, 2, 2], 2), -1)
+        self.assertEqual(self.first_larger([1, 1, 1, 2, 3, 4, 5], 1), 3)
+        self.assertEqual(self.first_larger([1, 2, 2, 2, 2, 2, 2, 3, 4, 5], 2), 7)
+        self.assertEqual(self.first_larger([1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 3, 4, 5], 2), 11)
+        self.assertEqual(self.first_larger([1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 3, 4, 4, 4, 4, 4, 5], 4), 17)
 
 if __name__ == '__main__':
     unittest.main()
