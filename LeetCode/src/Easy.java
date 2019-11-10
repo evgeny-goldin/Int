00259f4e18e87295e7b26346566fc06711ad0d64
Ev <evgenyg@gmail.com>
2019-11-10 07:42:28 +0000

    Edited Easy.java

--- LeetCode/src/Easy.java
+++ LeetCode/src/Easy.java
@@ -9,6 +9,24 @@ import java.util.Set;
 
 public class Easy {
 
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null) {
+            return l2;
+        }
+        if (l2 == null) {
+            return l1;
+        }    
+
+        if (l1.val < l2.val) {
+            l1.next = mergeTwoLists(l1.next, l2);
+            return l1;
+        } else {
+            l2.next = mergeTwoLists(l1, l2.next);
+            return l2;
+        }
+    }
+
+
     public ListNode reverseList2(ListNode head) {
         if ((head == null) || (head.next == null)) {
             return head;
