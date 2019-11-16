99185f8e2f6da4c10ece3e4ac402ebecb02fe143
Evgeny Goldin <evgenyg@gmail.com>
2019-11-16 00:56:26 +0000

    Edited Medium.java

--- LeetCode/src/Medium.java
+++ LeetCode/src/Medium.java
@@ -1,5 +1,102 @@
 class Solution {
     
+    // 445. Add Two Numbers II
+    
+    private final C EMPTY_C = new C(0, null);
+    
+    // Container for recursion returns
+    private class C {
+        private int carryOver;
+        private ListNode node;
+        private C (int carryOver, ListNode node) {
+            this.carryOver = carryOver;
+            this.node = node;
+        }
+    }
+    
+    // Adds two lists, *both pointers are equaly distant from their ends*
+    // Returns a new container with a carry over and a new list
+    private C add(ListNode l1, ListNode l2) {
+        C c           = ((l1.next == null) && (l2.next == null)) ? EMPTY_C : add(l1.next, l2.next);        
+        int sum       = l1.val + l2.val + c.carryOver;
+        int carryOver = (sum > 9) ? 1 : 0;
+        sum           = (sum > 9) ? sum - 10 : sum;
+        ListNode l    = new ListNode(sum);
+        l.next        = c.node;
+        
+        return new C(carryOver, l);
+    }
+    
+    // Calcualtes list's length
+    private int listLength (ListNode l) {
+        int length = 0;
+        for (; l != null; l = l.next){ length++; };
+        return length;
+    }
+    
+    // Applies carry over to the list (modifying it), returns a new carry over
+    private C addCarryOver(ListNode start, ListNode end, int carryOver) {
+        C c = (start == end) ? new C(carryOver, null) : addCarryOver(start.next, end, carryOver);
+        if (c.carryOver == 0) {
+            return EMPTY_C;            
+        } 
+        
+        start.val += c.carryOver;
+        int carry  = (start.val > 9) ? 1 : 0;
+        start.val  = (start.val > 9) ? start.val - 10 : start.val;
+        
+        return new C(carry, null);
+    }
+    
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        if ((l1 == null) || (l2 == null)) {
+            return (l1 == null) ? l2 : l1;
+        }
+        
+        int length1 = listLength(l1);
+        int length2 = listLength(l2);
+        
+        // Two lists are of identical length - easy case
+        if (length1 == length2) {
+            C c = add(l1, l2);
+
+            if (c.carryOver == 0) {
+                return c.node;
+            } 
+            
+            ListNode root = new ListNode(c.carryOver);
+            root.next = c.node;
+            return root;                
+        }
+        
+        ListNode longerList  = length1 > length2 ? l1 : l2;
+        ListNode shorterList = length1 > length2 ? l2 : l1;
+        ListNode l = longerList;
+        int steps = Math.abs(length1 - length2) - 1;
+    
+        // Moving l forward so that l.next and shorterList pointers are equally distant from the end
+        for (int j = 0; j < steps; j++) { l = l.next; }
+        C c = add(l.next, shorterList);
+        
+        // Attaching the result to the longer list, modifying it
+        l.next = c.node;
+        
+        if ((l.val + c.carryOver) < 10) {
+            l.val += c.carryOver;
+            return longerList;
+        }
+        
+        c = addCarryOver(longerList, l, c.carryOver); 
+        
+        if (c.carryOver == 0) {
+            return longerList;
+        } 
+        
+        ListNode root = new ListNode(c.carryOver);
+        root.next = longerList;
+        return root;                
+    }    
+    
     // 2. Add Two Numbers
     
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
