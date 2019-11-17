aebf9b99880255df98925c7834f1eef97c8f16bc
Ev <evgenyg@gmail.com>
2019-11-17 06:24:40 +0000

    Edited Medium.java

--- LeetCode/src/Medium.java
+++ LeetCode/src/Medium.java
@@ -2,6 +2,18 @@ class Solution {
     
     // 3. Longest Substring Without Repeating Characters
     
+    public int lengthOfLongestSubstring3(String s) {
+        int n = s.length(), ans = 0;
+        int[] index = new int[128]; // current index of character
+        // try to extend the range [i, j]
+        for (int j = 0, i = 0; j < n; j++) {
+            i = Math.max(index[s.charAt(j)], i);
+            ans = Math.max(ans, j - i + 1);
+            index[s.charAt(j)] = j + 1;
+        }
+        return ans;
+    }
+    
     private boolean isSet(long bitSet, int bit) {
         return ((bitSet << ~bit) < 0);
     }
