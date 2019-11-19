54f784d074a48f65d238e90566078081e83f6755
Evgeny <evgenyg@gmail.com>
2019-11-19 07:04:01 +0000

    Edited Medium.java

--- LeetCode/src/Medium.java
+++ LeetCode/src/Medium.java
@@ -1,5 +1,68 @@
 class Solution {
     
+    // 12. Integer to Roman - https://leetcode.com/problems/integer-to-roman/
+    
+    public String intToRoman(int n) {
+        StringBuilder b = new StringBuilder();
+        
+        for (int j = 0; j < (n / 1000); j++) {
+            b.append("M");
+        }
+        
+        n = n % 1000;
+        
+        if (n >= 900) {
+            b.append("CM");
+            n -= 900;
+        } else if (n >= 500) {
+            b.append("D");
+            n -= 500;
+        } else if (n >= 400) {
+            b.append("CD");
+            n -= 400;
+        } 
+        
+        for (int j = 0; j < (n / 100); j++) {
+            b.append("C");
+        }
+
+        n = n % 100;
+        
+        if (n >= 90) {
+            b.append("XC");
+            n -= 90;
+        } else if (n >= 50) {
+            b.append("L");
+            n -= 50;
+        } else if (n >= 40) {
+            b.append("XL");
+            n -= 40;
+        } 
+        
+        for (int j = 0; j < (n / 10); j++) {
+            b.append("X");
+        }
+        
+        n = n % 10;
+
+        if (n >= 9) {
+            b.append("IX");
+            n -= 9;
+        } else if (n >= 5) {
+            b.append("V");
+            n -= 5;
+        } else if (n >= 4) {
+            b.append("IV");
+            n -= 4;
+        } 
+
+        for (int j = 0; j < n; j++) {
+            b.append("I");
+        }
+        
+        return b.toString();
+    }
+    
     // 2. Add Two Numbers
     
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
