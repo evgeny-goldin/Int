5d44e82b97e3733f356d58fef4850957687b01a7
Evgeny Goldin <evgenyg@gmail.com>
2019-11-20 07:36:35 +0000

    Edited Medium.java

--- LeetCode/src/Medium.java
+++ LeetCode/src/Medium.java
@@ -1,5 +1,40 @@
 class Solution {
     
+ 
+    // 18. 4Sum - https://leetcode.com/problems/4sum/
+    
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        if ((nums == null) || (nums.length < 4)) {
+            return Collections.emptyList();
+        }       
+        
+        Arrays.sort(nums);
+        
+        List<List<Integer>> result = new ArrayList();
+        
+        for (int i = 0; i < (nums.length - 3); i++) {
+            if ((i > 0) && (nums[i] == nums[i-1])) { continue; } 
+            for (int j = i+1; j < (nums.length - 2); j++) {
+                if ((j > (i+1)) && (nums[j] == nums[j-1])) { continue; } 
+                
+                for (int left = j + 1, right = nums.length - 1; left < right;) {
+                    int sum = nums[i] + nums[j] + nums[left] + nums[right]; 
+                    if (sum == target) {
+                        result.add(Arrays.asList(nums[i], nums[j], nums[left], nums[right]));                           
+                        left++;
+                        for (; (left < right) && (nums[left] == nums[left-1]); left++);                        
+                    } else if (sum < target) {
+                        left++;
+                    } else {
+                        right--;
+                    }
+                }
+            }
+        }
+        
+        return result;
+    }
+    
     // 16. 3Sum Closest - https://leetcode.com/problems/3sum-closest/
     
     public int threeSumClosest(int[] nums, int target) {
