a03fb7f6f14fb2d0313d4c78a52dc381e49e9b66
Ev <evgenyg@gmail.com>
2019-11-21 11:21:32 +0000

    Edited Hard.java

--- LeetCode/src/Hard.java
+++ LeetCode/src/Hard.java
@@ -13,15 +13,18 @@ class Solution {
         int missingLetters = t.length(), left = 0, right = 0, start = -1, window = Integer.MAX_VALUE;
         
         while (right < s.length()) {
+          // Moving "right" until we find all letters and "missingLetters" drops to zero   
           if ((--counters[s.charAt(right++)]) > -1) {
               missingLetters--;
           }
             
           while (missingLetters == 0) {
+            // Checking the current window (from "left" to "right")  
             if ((right - left) < window) {
               start = left;
               window = right - left;
             }
+            // Moving "left" as long as we don't lose any letter by doing so ("missingLetters" stays zero)
             if ((++counters[s.charAt(left++)]) == 1) {
                 missingLetters++;
             }
