3b4beaab2f09763192720c2a1fd7eda6548f475f
Evgeny Goldin <evgenyg@gmail.com>
2019-11-22 08:55:14 +0000

    Edited Medium.java

--- LeetCode/src/Medium.java
+++ LeetCode/src/Medium.java
@@ -7,31 +7,17 @@ class Solution {
             return nums;
         }
         
-        // The length of the input array 
-        int length = nums.length;
-
-        // Final answer array to be returned
-        int[] answer = new int[length];
-
-        // answer[i] contains the product of all the elements to the left
-        // Note: for the element at index '0', there are no elements to the left,
-        // so the answer[0] would be 1
-        answer[0] = 1;
-        for (int i = 1; i < length; i++) {
-
-            // answer[i - 1] already contains the product of elements to the left of 'i - 1'
-            // Simply multiplying it with nums[i - 1] would give the product of all 
-            // elements to the left of index 'i'
-            answer[i] = nums[i - 1] * answer[i - 1];
+        int[] answer = new int[nums.length];
+        answer[0]    = 1;
+        
+        // Accumulate from the left
+        for (int i = 1, L = nums[0]; i < nums.length; i++) {
+            answer[i] = L;
+            L        *= nums[i];
         }
 
-        // R contains the product of all the elements to the right
-        // Note: for the element at index 'length - 1', there are no elements to the right,
-        // so the R would be 1
-        int R = 1;
-        for (int i = length - 1; i >= 0; i--) {
-            // For the index 'i', R would contain the 
-            // product of all elements to the right. We update R accordingly
+        // Accumulate from the right
+        for (int i = nums.length - 1, R = 1; i >= 0; i--) {
             answer[i] *= R;
             R         *= nums[i];
         }
