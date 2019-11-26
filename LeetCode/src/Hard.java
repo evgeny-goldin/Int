d2fd7c725e5ec8cffea32494648d1cee7782d9d9
Evgeny Goldin <evgenyg@gmail.com>
2019-11-26 05:05:08 +0000

    Edited Hard.java

--- LeetCode/src/Hard.java
+++ LeetCode/src/Hard.java
@@ -1,4 +1,65 @@
 class Solution {
+    
+    // 329. Longest Increasing Path in a Matrix - https://leetcode.com/problems/longest-increasing-path-in-a-matrix/
+    
+    private int[][] cache;
+    
+    private int dfs(int[][] m, int row, int column) {
+        
+        if (cache[row][column] > 0) {
+            return cache[row][column];
+        }
+
+        int rows = m.length;
+        int columns = m[0].length;
+        
+        int n  = m[row][column]; 
+        int n1 = (column > 0) ? m[row][column-1]           : 0;
+        int n2 = (row    > 0) ? m[row-1][column]           : 0;
+        int n3 = (column < (columns-1)) ? m[row][column+1] : 0;
+        int n4 = (row    < (rows-1)) ? m[row+1][column]    : 0;
+        
+        int result = 1;
+
+        if (n < n1) {
+            result = Math.max(result, 1 + dfs(m, row, column-1));
+        }
+        
+        if (n < n2) {
+            result = Math.max(result, 1 + dfs(m, row-1, column));
+        }
+        
+        if (n < n3) {
+            result = Math.max(result, 1 + dfs(m, row, column+1));
+        }
+
+        if (n < n4) {
+            result = Math.max(result, 1 + dfs(m, row+1, column));
+        }
+
+        cache[row][column] = result;
+        return result;
+    }
+    
+    public int longestIncreasingPath(int[][] m) {
+        if ((m == null) || (m.length < 1) || (m[0].length < 1)) {
+            return 0;
+        }       
+        
+        int rows = m.length;
+        int columns = m[0].length;
+        int result = 0;
+        cache = new int[rows][columns];
+        
+        for (int row = 0; row < rows; row++) {
+            for (int column = 0; column < columns; column++) {
+                result = Math.max(result, dfs(m, row, column));
+            }
+        }
+        
+        return result;
+    }
+    
 
     // 76. Minimum Window Substring - https://leetcode.com/problems/minimum-window-substring/
     
