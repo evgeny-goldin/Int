9ad45200d6bb38a6b8c5fc19311de848c778a76f
Evgeny Goldin <evgenyg@gmail.com>
2019-12-02 18:51:54 +0000

    Edited Easy.java

--- LeetCode/src/Easy.java
+++ LeetCode/src/Easy.java
@@ -9,6 +9,41 @@ import java.util.Set;
 
 public class Easy {
     
+    // 217. Contains Duplicate - https://leetcode.com/problems/contains-duplicate/
+    
+    public boolean containsDuplicate2(int[] nums) {
+        if ((nums == null) || (nums.length < 2)) {
+            return false;
+        }
+        
+        Set<Integer> set = new HashSet<>();
+
+        for (int num : nums) {
+            if (set.contains(num)) {
+                return true;
+            }
+            set.add(num);
+        }
+        
+        return false;
+    }
+    
+    public boolean containsDuplicate1(int[] nums) {
+        if ((nums == null) || (nums.length < 2)) {
+            return false;
+        }
+        
+        Arrays.sort(nums);
+        
+        for (int i = 0; i < (nums.length - 1); ++i) {
+            if (nums[i] == nums[i + 1]) {
+                return true;
+            }
+        }
+        
+        return false;
+    }
+    
     // 136. Single Number - https://leetcode.com/problems/single-number/
     
     public int singleNumber2(int[] nums) {
