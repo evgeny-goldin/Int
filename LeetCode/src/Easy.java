53f19577d37f72607d12b9b681c43dab1cfdab6e
Evgeny Goldin <evgenyg@gmail.com>
2019-12-02 06:59:44 +0000

    Edited Easy.java

--- LeetCode/src/Easy.java
+++ LeetCode/src/Easy.java
@@ -557,26 +557,7 @@ public class Easy {
         return maxSum; 
     } 
 
-    // 121. Best Time to Buy and Sell Stock (Easy)
-    
-    public int maxProfit2(int[] prices) {
-        if ((prices == null) || (prices.length < 2)) {
-            return 0;
-        }       
-        
-        int min = prices[0], result = 0;
-        
-        for (int j = 1; j < prices.length; j++) {
-            int price = prices[j];
-            if (price < min) {
-                min = price;
-            } else if ((price - min) > result) {
-                result = price - min;
-            }   
-        }
-        
-        return result;
-    }
+    // 121. Best Time to Buy and Sell Stock (Easy) - https://leetcode.com/problems/best-time-to-buy-and-sell-stock/submissions/
     
     public int maxProfit1(int[] prices) {
         if ((prices == null) || (prices.length < 2)) {
